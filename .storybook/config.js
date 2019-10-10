400be072e94a212fae1792f77cb187f30fecc095
jupegarnica <jupegarnica@gmail.com>
2019-10-10 15:27:47 +0200

    install addon accessibility

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react';
 import { addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import {  addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 
+// addon viewports
 const newViewports = {
   kindleFire2: {
     name: 'Kindle Fire 2',
@@ -18,13 +21,23 @@ const newViewports = {
     },
   },
 };
-
 addParameters({
   viewport: { viewports: {
       ...INITIAL_VIEWPORTS,
       newViewports
     }
  },
+});
+
+// addon accessibility
+addDecorator(withA11y);
+addParameters({
+  a11y: {
+    // ... axe options
+    element: '#root', // optional selector which element to inspect
+    config: {}, // axe-core configurationOptions (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1)
+    options: {}, // axe-core optionsParameter (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter)
+  },
 });
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
