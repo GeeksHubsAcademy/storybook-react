ccbb36859475be2c0a9a908d5c177e17a25a7ec2
jupegarnica <jupegarnica@gmail.com>
2019-10-10 16:22:48 +0200

    setup for geekshubs

---
+++ .storybook/theme.js
@@ -0,0 +1,38 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  // colorPrimary: 'hotpink',
+  // colorSecondary: 'deepskyblue',
+
+  // UI
+  // appBg: 'white',
+  // appContentBg: 'silver',
+  // appBorderColor: 'grey',
+  // appBorderRadius: 4,
+
+  // Typography
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: 'monospace',
+
+  // Text colors
+  // textColor: 'black',
+  // textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  // barTextColor: 'silver',
+  // barSelectedColor: 'black',
+  // barBg: 'hotpink',
+
+  // Form colors
+  // inputBg: 'white',
+  // inputBorder: 'silver',
+  // inputTextColor: 'black',
+  // inputBorderRadius: 4,
+
+  brandTitle: 'Geekshubs',
+  brandUrl: 'https://geekshubs.com/',
+  brandImage:
+    'https://geekshubs.com/wp-content/uploads/2019/02/logo_GHAcademy-Footer.svg',
+});
