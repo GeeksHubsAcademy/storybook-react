6875429f65015f0a98264981ad95df86c56e9d8b
jupegarnica <jupegarnica@gmail.com>
2019-10-10 15:24:24 +0200

    install addon viewports

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,30 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
+const newViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+};
+
+addParameters({
+  viewport: { viewports: {
+      ...INITIAL_VIEWPORTS,
+      newViewports
+    }
+ },
+});
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
