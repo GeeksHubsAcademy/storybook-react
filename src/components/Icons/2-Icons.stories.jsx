d0c3585b7d9079645bca725d0f98a99b3c909d30
jupegarnica <jupegarnica@gmail.com>
2019-10-14 15:24:20 +0200

    add decorator to icons

--- src/components/Icons/2-Icons.stories.jsx
+++ src/components/Icons/2-Icons.stories.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-
-
+import styled from 'styled-components';
 import Spinner from './Spinner';
 import Check from './Check';
 import Fail from './Fail';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow:row nowrap;
+  justify-content:center;
+  align-items:center;
+  color:black;
+  box-sizing:border-box;
+  min-height:90vh;
+  & > span {
+    height: 40px;
+    width: 40px;
+  }
+
+`;
+
+const decorator = storyFn => <Wrapper>{storyFn()}</Wrapper>;
 export default {
-  title: 'Icons/Icons',
+  title: 'Icons',
+  decorators: [decorator],
 };
 
-export const _Spinner = () => (
-         <div style={{width:'40px',height:'40px'}}>
-           <Spinner></Spinner>
-         </div>
-       );
-export const _Check = () => <Check></Check>
-export const _Fail = () => <Fail></Fail>
+export const _Spinner = () => <Spinner></Spinner>;
+export const _Check = () => <Check></Check>;
+export const _Fail = () => <Fail></Fail>;
