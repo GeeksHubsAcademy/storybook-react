6337458973adba9885ad4c4caf6d4fc6c796b56d
jupegarnica <jupegarnica@gmail.com>
2019-10-20 14:18:24 +0200

    build

---
+++ dist/layout/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"FlexContainer",{enumerable:true,get:function get(){return _FlexContainer.default}});var _FlexContainer=_interopRequireDefault(require("./FlexContainer.jsx"));function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}
