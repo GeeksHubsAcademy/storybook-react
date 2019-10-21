306f05f9d52b5a7ebb6cdd3913566b1f2a543185
jupegarnica <jupegarnica@gmail.com>
2019-10-21 12:21:45 +0200

    grid container componetent and stories created

--- src/layout/Flex.stories.jsx
+++ src/layout/Flex.stories.jsx
@@ -5,7 +5,7 @@ import FlexContainer from './FlexContainer';
 const FlexItem = (p) => <span {...p}  css={`background-color:purple; border: 2px black  solid; color:white;; padding:0.5em;`}/>;
 
 export default {
-  title: 'Layout/Flexbox!',
+  title: 'Layout/Flexbox',
 };
 
 const generateArray = (length, fn) => Array.from({ length }, fn);
