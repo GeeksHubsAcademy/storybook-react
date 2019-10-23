b6ce87153749126b113d77935512f6eb91e9276b
jupegarnica <jupegarnica@gmail.com>
2019-10-23 15:22:44 +0200

    build

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var message="\nImporta los componentes individualmente,  por ejemplo:\n\nimport Button from \"storybook-react-geekshubs/dist/components/Buttons/Button\";\n";console.warn(message);var _default=message;exports.default=_default;
