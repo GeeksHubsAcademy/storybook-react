cf066685b371f418e181460057c6ebc4fb29383d
jupegarnica <jupegarnica@gmail.com>
2019-10-23 15:04:07 +0200

    log message when using the library default import

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const message = `
+Importa los componentes individualmente,  por ejemplo:
+
+import Button from "storybook-react-geekshubs/dist/components/Buttons/Button";
+`;
+
+console.warn(message);
+
+export default message;
