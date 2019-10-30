be99d26d77be2e82bacec4677ea9bc556a905820
juan <jupegarnica@gmail.com>
2019-10-30 17:03:25 +0100

    logger not imported

--- src/0-readme.stories.jsx
+++ src/0-readme.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import readmeTxt from '../readme.md';
 import styled from 'styled-components';
-import loggerService from './servicies/logger.service';
+// import loggerService from './servicies/logger.service';
 
 const Wrapper = styled.pre`
   font-size: 0.8em;
@@ -14,7 +14,7 @@ export default {
   decorators: [decorator],
 };
 
-loggerService.log('hola');
-loggerService.error('hola');
+// loggerService.log('hola');
+// loggerService.error('hola');
 
 export const readme = () => readmeTxt;
