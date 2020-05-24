2d023096d0e93cd48a28e96d60450a59701d7c15
juan <jupegarnica@gmail.com>
2020-05-24 19:59:46 +0200

    build

--- dist/servicies/singleton.service.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}var Singleton=function Singleton(){_classCallCheck(this,Singleton)};var _default=new Singleton;exports.default=_default;
